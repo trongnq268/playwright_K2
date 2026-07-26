4bdc08b63e0ae1941cd38c4721b02a1e3437ed04
trongnq <trongnguyen268@gmail.com>
2026-07-26 16:20:26 +0700

    update

--- tests/homework.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("test", async ({ page }) => {
-  await page.getByText('firefox').click();
-  await page.getByRole('link', { name: 'GitHub repository' })
-
-  
-});

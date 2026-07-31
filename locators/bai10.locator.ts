5f145d2d804a9495bd45cd57368216219bb6c93d
Nguyen Duc An <anamanh310@gmail.com>
2026-07-31 20:02:50 +0700

    1

--- locators/bai10.locator.ts
+++ locators/bai10.locator.ts
@@ -8,9 +8,16 @@ export const Locator = (page: Page) => ({
 
 go_to_URL: 'https://automationexercise.com',
 button_home_page:page.getByRole('link', { name: 'Home' }),
+button_product:page.locator(`a[href="/products"]`),
+search_input: page.locator('#search_product'),
+search_button: page.locator('#submit_search'),
 button_cart:page.getByRole('link', { name: 'Cart' }),
 
-
+find_product_by_name: async (name: string) => {
+    await page.locator('#search_product').fill(name);
+    await page.locator('#submit_search').click();
+    return page.locator('.productinfo', { hasText: name });
+},
 
 
 });  
--- tests/bai10.spec.ts
+++ tests/bai10.spec.ts
@@ -13,11 +13,14 @@ test.beforeEach(async ({ page }, testInfo) => {
     await expect(locator.button_home_page).toBeVisible();
     
 }); 
-test('testcase1', async ({ page }) => {
- 
-   
+test('tìm sản phẩm theo tên', async ({ page }) => {
+    const locator = Locator(page);
 
-    
-    
-    
+    await page.goto(locator.go_to_URL);
+    await expect(locator.button_home_page).toBeVisible();
+
+    await locator.button_product.click();
+    const result = await locator.find_product_by_name('Blue Top');
+
+    await expect(result).toBeVisible();
 });

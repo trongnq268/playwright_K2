d943a7bc377215d3a80782dc84ed2f3079e176c4
Nguyen Duc An <anamanh310@gmail.com>
2026-08-04 23:38:48 +0700

    aa

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries:  2,
   /* Opt out of parallel tests on CI. */
-  workers: 4,
+  workers: 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */

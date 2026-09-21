d428c0364ccf90d1b988e947f4d4de5282e9f6fc
trongnq268 <70145531+trongnq268@users.noreply.github.com>
2026-09-21 20:46:04 +0700

    Update test.spec.ts

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -7,20 +7,20 @@ const wait = (ms: number): Promise<void> => {
 test('Demo 4: Async/Await giúp code chờ đúng thứ tự', async () => {
   const steps: string[] = [];
   console.log('1. Bắt đầu mở giỏ hàng...');
-  console.log('5. Tiến hành thanh toán.');
+  console.log('15. Tiến hành thanh toán.');
   steps.push('start');
 
   await wait(3000); // Chờ 3 giây
 
-  console.log('6. Đã tải xong danh sách!');
-  console.log('6. Tiến hành thanh toán.');
-  console.log('36. Tiến hành thanh toán.');
+  console.log('16. Đã tải xong danh sách!');
+  console.log('16. Tiến hành thanh toán.');
+  console.log('136. Tiến hành thanh toán.');
   steps.push('loaded');
-  console.log('5. Tiến hành thanh toán.');
-  console.log('5. Bắt đầu mở giỏ hàng...');
-  console.log('5. Bắt đầu mở giỏ hàng...');
-  console.log('6. Tiến hành thanh toán.');
-  console.log('16. Bắt đầu mở giỏ hàng...');
+  console.log('15. Tiến hành thanh toán.');
+  console.log('15. Bắt đầu mở giỏ hàng...');
+  console.log('15. Bắt đầu mở giỏ hàng...');
+  console.log('16. Tiến hành thanh toán.');
+  console.log('116. Bắt đầu mở giỏ hàng...');
   console.log('61. Bắt đầu mở giỏ hàng...');
   steps.push('checkout');
 
